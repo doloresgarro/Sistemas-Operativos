fafa64edd099e7c1d39afce99b9e09671718718d
Dolores Garro <129607981+doloresgarro@users.noreply.github.com>
2023-09-24 11:38:12 -0300

    Add files via upload

--- Clase 3/Ejercicio17.sh
+++ Clase 3/Ejercicio17.sh
@@ -1,7 +1,13 @@
-#!/bin/bash 
+#!/bin/bash
 
 for archivo in *; do
-	if [ -f $file  ]; then
-		echo "$file" | tr 'a-zA-z' 'A-Za-z' | tr -d 'Aa'
+	if [ -f "$archivo"  ]; then
+		nuevo_nom=$(echo "$archivo" | tr 'a-zA-Z' 'A-Za-z' | tr -d 'Aa')
+		mv "$archivo" "$nuevo_nom"
+		echo "$nuevo_nom"
 	fi
 done
+
+# tr 'a-zA-Z' 'A-Za-z'--> convierte minusculas en mayusculas y viceversa
+
+#  tr -d 'Aa' --> elimina la letra "a" del nombre del archivo
